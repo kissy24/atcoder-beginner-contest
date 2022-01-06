4ac3e7c4c218057025af3643afc9e6d912e3d48d
kissy24 <kissy105808j@gmail.com>
2022-01-06 22:29:54 +0900

    generate_dirの指定先を新しいフォルダ構成に修正しました

---
+++ generate_dir.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+def main():
+    Path(tgt_dir := f"./ABC_{input()}").mkdir()
+    for problem in ["a", "b"]:
+        Path(f"{tgt_dir}/{problem}.py").touch(exist_ok=False)
+
+
+if __name__ == "__main__":
+    main()
